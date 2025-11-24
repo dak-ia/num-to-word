5dd80c8d8f7be00e04ca563d1e6bf163f4135db9
dak-ia <97687749+dak-ia@users.noreply.github.com>
2025-11-25 03:52:50 +0900

    chore: jest導入

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  testEnvironment: 'node',
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    '**/*.{js,jsx}',
+    '!**/node_modules/**',
+  ],
+  testMatch: [
+    '**/__tests__/**/*.js',
+    '**/?(*.)+(spec|test).js',
+  ],
+};
