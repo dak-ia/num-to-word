44cba7de9c81c4f85eb9696f317c836993175e7c
dak-ia <97687749+dak-ia@users.noreply.github.com>
2025-12-12 05:17:03 +0900

    feat: typescript導入

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,10 @@
 import globals from "globals";
 import js from "@eslint/js";
+import tseslint from "typescript-eslint";
 
 export default [
   js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     languageOptions: {
       ecmaVersion: "latest",
--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,14 @@ export default defineConfig({
   },
   build: {
     lib: {
-      entry: "./src/index.js",
+      entry: "./src/index.ts",
       name: "NumToWord",
-      formats: ["es", "cjs"],
-      fileName: (format) => `NumToWord.${format === "es" ? "mjs" : "cjs"}`,
+      formats: ["es", "cjs", "umd"],
+      fileName: (format) => {
+        if (format === "es") return "index.mjs";
+        if (format === "cjs") return "index.cjs";
+        if (format === "umd") return "index.umd.js";
+      },
     },
     outDir: "dist",
     emptyOutDir: true,
