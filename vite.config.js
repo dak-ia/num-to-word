d2c902dc55dbaf71cea8adc77e091abe481cb8ad
dak-ia <97687749+dak-ia@users.noreply.github.com>
2026-01-21 05:19:29 +0900

    chore: 動作確認用のHTML周りの修正 (#29)
    * chore: githubのURL修正
    
    * chore: 開発サーバー用にrootを設定
    
    * chore: ローカルで直接htmlを開くだけでもビルド物を読み込めるようにする
    
    * docs: update readme

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,42 @@
 import { defineConfig } from "vite";
 import pkg from "./package.json";
 
-export default defineConfig({
-  define: {
-    VERSION: JSON.stringify(pkg.version),
-  },
-  build: {
-    lib: {
-      entry: "./src/index.ts",
-      name: "NumToWord",
-      formats: ["es", "cjs", "umd"],
-      fileName: (format) => {
-        if (format === "es") return "index.mjs";
-        if (format === "cjs") return "index.cjs";
-        if (format === "umd") return "index.umd.js";
-      },
+export default defineConfig(({ command }) => {
+  const commonConfig = {
+    define: {
+      VERSION: JSON.stringify(pkg.version),
     },
-    outDir: "dist",
-    emptyOutDir: true,
-  },
+  };
+
+  if (command === "serve") {
+    // dev server
+    return {
+      ...commonConfig,
+      root: "examples",
+      server: {
+        watch: {
+          usePolling: true, // wsl
+        },
+      },
+    };
+  } else {
+    // build
+    return {
+      ...commonConfig,
+      build: {
+        lib: {
+          entry: "./src/index.ts",
+          name: "NumToWord",
+          formats: ["es", "cjs", "umd"],
+          fileName: (format) => {
+            if (format === "es") return "index.mjs";
+            if (format === "cjs") return "index.cjs";
+            if (format === "umd") return "index.umd.js";
+          },
+        },
+        outDir: "dist",
+        emptyOutDir: true,
+      },
+    };
+  }
 });
