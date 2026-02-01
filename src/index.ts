bad5b717c21356b9f40004a74573da7af313fd3e
dak-ia <97687749+dak-ia@users.noreply.github.com>
2026-02-02 01:09:31 +0900

    feat: named exportにも対応 (#34)
    * feat: named exportにも対応
    
    * test: named exportのテストを追加

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,14 @@ declare const VERSION: string;
 
 const version: string = VERSION;
 
+// named export
+export const numToWordEn = toEn;
+export const numToWordJp = toJp;
+export const numToWordJpDaiji = toJpDaiji;
+export const numToWordSi = toSi;
+export const numToWordLocale = toLocaleString;
+
+// default export
 export default {
   version,
   toEn,
