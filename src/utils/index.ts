861d3e39a8fa41907dde06572e63ddd9c91bdaa4
dak-ia <97687749+dak-ia@users.noreply.github.com>
2026-08-23 11:26:41 +0900

    feat: 桁読み用の共通処理追加

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
 export { preprocessNumber, splitTo1Digit, splitTo3Digits, splitTo4Digits } from "./helpers";
+export { replaceDigits } from "./replaceDigits";
 export { replaceIntUnitEn } from "./replaceIntUnitEn";
 export { replaceIntUnitJp } from "./replaceIntUnitJp";
